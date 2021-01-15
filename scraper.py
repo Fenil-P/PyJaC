25d2d5e8eff36bdacff1ba3be261b3e2f6468d82
Zereck12 <saadafridi2001@gmail.com>
2021-01-15 17:54:04 -0500

    Finished GUI

--- scraper.py
+++ scraper.py
@@ -1,37 +1,69 @@
+""" 
+Web Scraper for anime from MAL [myanimelist]. Given some string input,
+searches it on the [myanimelist] website and returns details 
+regarding the closest matching anime
+"""
+
 import requests
 from bs4 import BeautifulSoup
+from typing import List
 
-def details(soup):
+
+class MAL: 
+    """ 
+    A MAL object that keeps track of it's anime's details
+    that are scraped off of [myanimelist.net]
+    """
+    link: str
+    title: str
+    desc: str
+    imageUrl: str
+    score: str
+    rank: str
+    popular: str
+    
+    def __init__(self, details: List[str]) -> None:
+        """ Initalize a MAL object """
+        self.link = details[0]
+        self.title = details[1]
+        self.desc = details[2][:-27]
+        self.imageUrl = details[3]
+        self.score = details[4]
+        self.rank = details[5]
+        self.popular = details[6]
+           
+    
+def details(soup: BeautifulSoup) -> List[str]:
+    """ Given <soup> figure out different properties of
+    anime and return them in a list"""
+    
     title = soup.find('h1', class_='title-name h1_bold_none').getText()
-    print("\nName of the Anime : ", title,"\n") 
     description = soup.find('p', {'itemprop': 'description'}).getText()
-    print('Description :  \n' + description)
     imageUrl = soup.find('img', alt=title)['data-src']
-    print('Image link :  ' + imageUrl)
     score = soup.find('div', class_='score-label').getText()
-    print('MyAnimeList Score :  ' + score)
     rank = soup.find('span', class_='numbers ranked').getText()
-    print(rank)
     popularity = soup.find('span', class_='numbers popularity').getText()
-    print(popularity)
+    
+    return [title, description, imageUrl, score, rank, popularity]
 
 
-def search():
-    anime_name = input("--> Enter the name of the Anime : ")
+def search(anime_name: str) -> MAL:
+    """ Given <anime_name> searches it on my anime list
+    and returns the details about the closest matching
+    anime """
+
     search_url = ("https://myanimelist.net/search/all?q=" + anime_name +"&cat=anime")
     source_code = requests.get(search_url)
     content = source_code.content
-    global soup
+    
     soup = BeautifulSoup(content,features="html.parser")
     link = soup.find('a', class_='hoverinfo_trigger fw-b fl-l')['href']
 
     actualPage = requests.get(link)
     actualContent = actualPage.content
     goodSoup = BeautifulSoup(actualContent,features="html.parser")
-    # print (goodSoup)
 
-    print('Closest Matching Anime :  ' + link)
-    details(goodSoup)
+    return MAL([link] + details(goodSoup)) 
 
 if __name__ == "__main__":
-    search()
+    print(search("naruto"))
